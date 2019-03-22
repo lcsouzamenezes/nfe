bb9dd26cf1c27d4e508112339e78080f98ca77e2
Paulinhoibti <paulo.mendes00@hotmail.com>
2019-03-22 17:22:49 -0300

    [UPDATE] Criação da view para cadastrar Nfes #13

---
+++ resources/views/cadastrar.blade.php
@@ -0,0 +1,8 @@
+<html>
+<body>
+    <form action="gerar-nfe" method="POST">
+        Numero Nota Fiscal:  <input type="text" name="nNF"/>
+        <input type="submit" name="submit" value="Me aperte!" />
+    </form>
+</body>
+</html>
