ab0d355a332373240a184da73cc977c0fa11f58b
wouerner <wouerner@protonmail.com>
2019-04-08 15:51:24 -0300

    [ADD] suporte ao CORS

--- app/Nfe/Http/Middleware/CorsMiddleware.php
+++ app/Nfe/Http/Middleware/CorsMiddleware.php
@@ -9,21 +9,19 @@ class CorsMiddleware
     public function handle($request, Closure $next)
     {
         $headers = [
-            'Access-Control-Allow-Origin'      => '*',
-            'Access-Control-Allow-Methods'     => 'POST, GET, OPTIONS, PUT, DELETE',
+            'Access-Control-Allow-Origin' => '*',
+            'Access-Control-Allow-Methods' => 'POST, GET, OPTIONS, PUT, DELETE',
             'Access-Control-Allow-Credentials' => 'true',
-            'Access-Control-Max-Age'           => '86400',
-            'Access-Control-Allow-Headers'     => 'Access-Control-Allow-Origin, Content-Type, Authorization, X-Requested-With'
+            'Access-Control-Max-Age' => '86400',
+            'Access-Control-Allow-Headers' => 'Access-Control-Allow-Origin,Content-Type,Authorization,X-Requested-With'
         ];
 
-        if ($request->isMethod('OPTIONS'))
-        {
+        if ($request->isMethod('OPTIONS')) {
             return response()->json('{"method":"OPTIONS"}', 200, $headers);
         }
 
         $response = $next($request);
-        foreach($headers as $key => $value)
-        {
+        foreach ($headers as $key => $value) {
             $response->header($key, $value);
         }
 
