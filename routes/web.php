4f66a851325338e448b203f0a73550c5c89c6479
Paulo Henrique <paulotux.mendes@gmail.com>
2019-05-13 11:29:32 -0300

    [UPDATE] Renomeando arquivos de Listar para Download. #56

--- routes/web.php
+++ routes/web.php
@@ -69,4 +69,4 @@ $router->get(
 
 $router->post('/criar-nfe/{ambiente}','\App\Nfe\Http\Controllers\CriarNotaFiscal@index');
 
-$router->get('/listar-xml/{id}','\App\Nfe\Http\Controllers\ListarXmlNotaFiscal@index');
+$router->get('/download-xml/{id}','\App\Nfe\Http\Controllers\DownloadXmlNotaFiscal@index');
