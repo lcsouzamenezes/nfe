83061cf1f5ee1435b07162b4ae1ad8f10c5dd33a
wouerner <wouerner@protonmail.com>
2019-05-13 16:23:31 -0300

    [ADD] ajuste na colsuta de xml

--- app/Nfe/Http/Controllers/ListarXmlNotaFiscal.php
+++ app/Nfe/Http/Controllers/ListarXmlNotaFiscal.php
@@ -41,4 +41,31 @@ class ListarXmlNotaFiscal
 
         echo $result['Body'];
     }
+
+    public function get(Request $request, $chNFe)
+    {
+        $xmlId = NfeModel::where('chNFe', $chNFe)->get()->toArray();
+        $keyXml = $xmlId[0]['data']['NFe']['infNFe']['attributes']['Id'].'.xml';
+
+        $s3 = new \Aws\S3\S3Client([
+            'version' => 'latest',
+            'region'  => env("S3_REGION"),
+            'endpoint' => env('MINIO_ENDPOINT', 'http://minio:9000'),
+            'use_path_style_endpoint' => true,
+            'credentials' => [
+                'key'    => env('S3_KEY'),
+                'secret' => env('S3_SECRET'),
+            ],
+        ]);
+
+        $result = $s3->getObject([
+            'Bucket' => env('S3_BUCKET'),
+            'Key'    => $keyXml,
+        ]);
+
+        header("Content-Type: {$result['ContentType']}");
+        header('Content-Disposition: attachment; filename="'.$keyXml.'" ');
+
+        echo $result['Body'];
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -70,3 +70,5 @@ $router->get(
 $router->post('/criar-nfe/{ambiente}','\App\Nfe\Http\Controllers\CriarNotaFiscal@index');
 
 $router->get('/listar-xml/{id}','\App\Nfe\Http\Controllers\ListarXmlNotaFiscal@index');
+
+$router->get('/download-local/{chNfe}','\App\Nfe\Http\Controllers\ListarXmlNotaFiscal@get');
